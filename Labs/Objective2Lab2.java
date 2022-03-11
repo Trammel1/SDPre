834e88b94c2c6f81771672c2300ccab08a0f1b7d
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-11 13:55:30 -0500

    Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Trammel Clark";
+    age = 27;
+    state = "Florida";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
