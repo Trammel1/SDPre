6331e9a9157bacf8448a1069c8f842f667a41559
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-11 17:17:19 -0500

    Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,15 @@
+public class Objective2Lab3 {
+  public static void main(String[] args){
+    //declaring variables
+    double num1, num2, num3, average;
+
+    //initializing variables
+    num1 = 10;
+    num2 = 7;
+    num3 = 3;
+    average = (num1 + num2 + num3) / 3;
+
+    //output
+    System.out.println(average);
+  }
+}
