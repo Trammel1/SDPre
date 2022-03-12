22c69db983ce6ac59fb0070d1ff217e26f89234e
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-12 06:29:08 -0500

    Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenuse;
+// Init variables
+    side1 = 52;
+    side2 = 32;
+// Calculate hypotenuse
+    hypotenuse = Math.sqrt((side1 * side1) + (side2 * side2));
+// Print result
+    System.out.println(hypotenuse);
+  }
+}
