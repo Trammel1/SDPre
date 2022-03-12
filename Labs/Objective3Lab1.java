9172ff677868c910dea5bc9c56c6414fc2245192
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-11 19:42:37 -0500

    Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args){
+    //declaring variables... again
+    double num1, num2, num3, average;
+
+    //initializing variables
+    num1 = 10;
+    num2 = 42;
+    num3 = 7;
+    average = (num1 + num2 + num3) / 3;
+
+    //output - is this called the method? or is that this whole section?
+    System.out.println(average);
