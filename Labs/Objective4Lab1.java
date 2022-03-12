1f895a2cdefce066b907876c669819abc81a0996
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-12 07:22:11 -0500

    Objective4Lab1.java

--- Labs/Objective4Lab1.java
+++ Labs/Objective4Lab1.java
@@ -11,6 +11,7 @@ public class Objective4Lab1{
       String fname = "";
       String firstname = "";
       String lname = "";
+      String lastname = "";
       String favoriteAnimal = "";
       String favoriteFood = "";
       String favoriteSong = "";
@@ -19,24 +20,36 @@ public class Objective4Lab1{
       System.out.println("What is your first name?");
 //input from keyboard scanner to add information to variables
       fname = keyboard.nextLine();
+
       System.out.println("Trammel");
       firstname = keyboard.nextLine();
 
       System.out.println("What is your last name?");
       lname = keyboard.nextLine();
 
+      System.out.println("Clark");
+      lastname = keyboard.nextLine();
+
       System.out.println("What is your favorite animal?");
       favoriteAnimal = keyboard.nextLine();
+
+      System.out.println("Tiger");
+      animal = keyboard.nextLine();
 
       System.out.println("What is your favorite food?");
       favoriteFood = keyboard.nextLine();
 
+      System.out.println("Hot Wings");
+      food = keyboard.nextLine();
+
       System.out.println("What is your favorite song?");
       favoriteSong = keyboard.nextLine();
 
+      System.out.println("Thats Hard");
+      answer = keyboard.nextLine();
+
 //final output
-      System.out.println("My name is Trammel" + fname + " " + lname);
-      System.out.println("Trammel" + firstname);
+      System.out.println("My name is Trammel Clark" + fname + " " + lname);
       System.out.println("My favorite animal is the Tiger" + favoriteAnimal);
       System.out.println("My favorite food is Hot Wings" + favoriteFood);
       System.out.println("Such a difficult question" + favoriteSong);
