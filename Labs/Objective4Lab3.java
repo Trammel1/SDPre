b715efe835c320c8919b043be07f19958f1a95cd
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-13 03:29:48 -0400

    Objective4Lab3.java

--- Labs/Objective4Lab3.java
+++ Labs/Objective4Lab3.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 
-public class Objective4Lab3 {
+public class Objective4Lab3{
   public static void main(String[] args) {
     int birthYear, age, currentYear;
 
     birthYear = 0;
-
     Scanner input = new Scanner(System.in);
     System.out.println("How old are you?");
-
     age = input.nextInt();
     currentYear = 2022;
-    birthYear = (currentYear - age );
+    birthYear = currentYear - age;
 
-    System.out.println("You were born in " birthYear ", ?");
-
+    System.out.println("You were born in " + birthYear);
     input.close();
+
   }
 }
