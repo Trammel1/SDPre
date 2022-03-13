089e5d05a60dab7158574c9c8aad83458b0319dd
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-13 03:40:40 -0400

    Objective5Lab1.java

--- Labs/Objective5Lab1.java
+++ Labs/Objective5Lab1.java
@@ -1,8 +1,8 @@
 public class Objective5Lab1 {
   public static void main(String[] args) {
 
-    int num1 = 7;
-    int num2 = 5;
+    int num1 = 5;
+    int num2 = 15;
 
     if (num1 > num2) {
       System.out.println(num1 + " is greater than " + num2);
