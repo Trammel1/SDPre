5d309ad7ecfdfb78e476389460db93a79a75d248
Trammel1 <100053072+Trammel1@users.noreply.github.com>
2022-03-13 04:36:46 -0400

    Objective7Lab4.java

--- Labs/Objective7Lab4.java
+++ Labs/Objective7Lab4.java
@@ -1,11 +1,12 @@
 public class Objective7Lab4 {
   public static void main(String[] args) {
-    int count = 0, sum = 0;
+    int count = 1;
+    int sum = 0;
 
     while(count <= 20) {
-      sum += count;
-      System.out.println( "Count: " + count + " | " + "Sum: " + sum );
-      count += 1;
+      sum = sum +count;
+      count++;
+      System.out.println(sum);
     }
   }
 }
